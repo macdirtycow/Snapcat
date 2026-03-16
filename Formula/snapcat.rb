f79f1b6a35b0d6898fd6c005fd54cf6db8133724
Leopold <leopold@Leopolds-MacBook-Air.local>
2026-03-16 02:21:13 +0100

    Fix formula - correct binary path

--- Formula/snapcat.rb
+++ Formula/snapcat.rb
@@ -1,11 +1,11 @@
 class Snapcat < Formula
-  desc "Lightweight macOS screenshot tool"
+  desc "Lightweight macOS screenshot tool with Command+Shift+S hotkey"
   homepage "https://github.com/macdirtycow/snapcat"
   url "https://github.com/macdirtycow/snapcat/releases/download/1.0.0/snapcat-1.0.0.zip"
   sha256 "25557d7652491aa6e34d454d1d73faca686e375de701da4481869120bc135ec0"
   license "MIT"
 
   def install
-    bin.install "snapcat"
+    bin.install ".build/release/snapcat"
   end
 end
