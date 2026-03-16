1b01bd55a37205f8ba90e15460b66ba6ee361db2
macdirtycow <macdirtycow@gmail.com>
2026-03-16 01:15:15 +0100

    Create main.swift file for macOS Snapcat app entry point

---
+++ Sources/main.swift
@@ -0,0 +1,14 @@
+// Main entry point for the Snapcat macOS application
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
