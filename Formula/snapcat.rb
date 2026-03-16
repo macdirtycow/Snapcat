289315bb103b65a312038e2811c288e211b5a30f
Leopold <leopold@Leopolds-MacBook-Air.local>
2026-03-16 02:09:18 +0100

    Fix: add correct SHA256 to formula

--- Formula/snapcat.rb
+++ Formula/snapcat.rb
@@ -1,7 +1,7 @@
 class Snapcat < Formula
-  desc "Lightweight macOS screenshot tool with Command+Shift+S hotkey"
-  homepage "https://github.com/macdirtycow/Snapcat"
-  url "https://github.com/macdirtycow/Snapcat/releases/download/v1.0.0/snapcat-1.0.0.zip"
+  desc "Lightweight macOS screenshot tool"
+  homepage "https://github.com/macdirtycow/snapcat"
+  url "https://github.com/macdirtycow/snapcat/releases/download/1.0.0/snapcat-1.0.0.zip"
   sha256 "25557d7652491aa6e34d454d1d73faca686e375de701da4481869120bc135ec0"
   license "MIT"
 
